9454465129f82fd9f3e2ff972ebe4f574b1505c2
Hynek Zemanec <zemanech@hotmail.com>
2020-04-19 17:34:07 +0200

    Adding brief description and links to demo, tweaking navigation component in wc

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# web-component-thesis
-Prototype application for the purposes of thesis that deals with Web Components, React and their combination in modern web apps 
+# Web Components in React case study 
+
+This project serves as a case study of the current state of the [web components standard](https://developer.mozilla.org/en-US/docs/Web/Web_Components). 
+It accompanies the bachelor thesis **Výhody a nevýhody použití webových komponent při vývoji moderních webových aplikací** (*Advantages and disadvantages of the use of web components for development of modern web applications*), at the University of Economics, Prague.
+
+## Contents
+Project includes 3 ditinct applications.
+
+1. [React app](./react) bootstraped with *create-react-app* ([demo](https://veterinamaj-react.netlify.app/))
+2. [App](./web-components) using exclusively *Web Components* ([demo](https://veterinamaj-wc.netlify.app/))
+3. [Integrated](./react_web-components) web components within React environment ([demo](https://veterinamaj-wcinreact.netlify.app/))
+
+Each app implements 5 interactive components.
+
+1. Navigation 
+2. Form
+3. Gallery
+4. Modal 
+5. Map
+
+## Getting started
+
+See `README.md` of the corresponsing project for getting started guide.
+
+## Bugs 🐛
+
+File an issue, send me PR or contact me at hynek@hynekzemanec.cz
--- web-components/src/components/VetNavigation/index.js
+++ web-components/src/components/VetNavigation/index.js
@@ -1,11 +1,11 @@
-import styles from './styles.scss'
-import gsap from 'gsap';
+import styles from "./styles.scss";
+import gsap from "gsap";
 
 const links = [
-    {
-        targetId: "hours",
-        label: "Ordinační hodiny",
-        svg: `<svg
+  {
+    targetId: "hours",
+    label: "Ordinační hodiny",
+    svg: `<svg
             class="svg-icon"
             enable-background="new 0 0 512 512"
             version="1.1"
@@ -23,12 +23,12 @@ const links = [
             <circle cx="125" cy="384" r="20" />
             <circle cx="212" cy="384" r="20" />
             <circle cx="212" cy="210" r="20" />
-            </svg>`
-    },
-    {
-        targetId: "about",
-        label: "Služby",
-        svg: `<svg
+            </svg>`,
+  },
+  {
+    targetId: "about",
+    label: "Služby",
+    svg: `<svg
         class="svg-icon"
         enable-background="new 0 0 512 512"
         version="1.1"
@@ -41,12 +41,12 @@ const links = [
         <path d="m407.36 171.91c-38.132 0-69.145 31.013-69.145 69.139 0 38.12 31.013 69.139 69.139 69.139s69.139-31.019 69.145-69.139c-1e-3 -38.126-31.019-69.139-69.139-69.139zm-6e-3 100.91c-17.521 0-31.773-14.251-31.773-31.766s14.251-31.766 31.773-31.766c17.515 0 31.766 14.251 31.766 31.766s-14.251 31.766-31.766 31.766z" />
         <path d="m112.11 0c-10.321 0-18.686 8.365-18.686 18.686v39.864c0 10.321 8.365 18.686 18.686 18.686s18.686-8.365 18.686-18.686v-39.864c0-10.321-8.365-18.686-18.686-18.686z" />
         <path d="M256.62,0c-10.321,0-18.686,8.365-18.686,18.686V58.55c0,10.321,8.365,18.686,18.686,18.686s18.686-8.365,18.686-18.686    V18.686C275.306,8.365,266.941,0,256.62,0z" />
-        </svg>`
-    },
-    {
-        targetId: "contact",
-        label: "Kontakt",
-        svg: `<svg
+        </svg>`,
+  },
+  {
+    targetId: "contact",
+    label: "Kontakt",
+    svg: `<svg
         class="svg-icon"
         enable-background="new 0 0 511.874 511.874"
         version="1.1"
@@ -57,13 +57,12 @@ const links = [
         <path d="m460.72 52.323-142.62 108.16c-4.035 2.932-9.499 2.932-13.534 0l-142.56-108.16c-3.756-2.849-9.111-2.113-11.959 1.643-2.849 3.756-2.113 9.111 1.643 11.959l142.58 108.15c10.144 7.494 23.989 7.494 34.133 0l142.63-108.15c1.805-1.368 2.992-3.398 3.299-5.642s-0.29-4.518-1.661-6.321c-2.848-3.749-8.194-4.482-11.947-1.639z" />
         <path d="m238.52 174.79-87.415 93.611c-3.214 3.447-3.025 8.848 0.422 12.062s8.848 3.025 12.062-0.422l87.416-93.653c2.888-3.484 2.553-8.617-0.762-11.698-3.316-3.081-8.46-3.037-11.723 0.1z" />
         <path d="m384.73 174.79c-3.214-3.447-8.614-3.637-12.062-0.422-3.447 3.214-3.637 8.614-0.422 12.062l87.39 93.611c3.214 3.447 8.614 3.637 12.062 0.422 3.447-3.214 3.637-8.614 0.422-12.062l-87.39-93.611z" />
-        </svg>`
-
-    },
-    {
-        targetId: "end",
-        label: "Kudy k nám",
-        svg: `<svg
+        </svg>`,
+  },
+  {
+    targetId: "end",
+    label: "Kudy k nám",
+    svg: `<svg
         class="svg-icon"
         enable-background="new 0 0 426.667 426.667"
         version="1.1"
@@ -72,114 +71,116 @@ const links = [
         xmlns="http://www.w3.org/2000/svg"
             >
             <path d="m213.33 0c-82.453 0-149.33 66.88-149.33 149.33 0 112 149.33 277.33 149.33 277.33s149.33-165.33 149.33-277.33c1e-3 -82.453-66.879-149.33-149.33-149.33zm0 202.67c-29.44 0-53.333-23.893-53.333-53.333s23.893-53.334 53.333-53.334 53.333 23.893 53.333 53.333-23.893 53.334-53.333 53.334z" />
-        </svg>`
-    }
+        </svg>`,
+  },
 ];
 
-const template = document.createElement('template');
+const template = document.createElement("template");
 template.innerHTML = `
     <style>${styles.toString()}</style>
     <nav></nav>
-`
+`;
 
-class VetNavigation extends HTMLElement{
-    constructor() {
-        super();
+class VetNavigation extends HTMLElement {
+  constructor() {
+    super();
 
-        this.attachShadow({mode:'open'})
-        // create shadowRoot and append nav
-        this.shadowRoot.appendChild(template.content.cloneNode(true))
-        this.nav = this.shadowRoot.querySelector('nav');
-        this.activeLink = null;
+    this.attachShadow({ mode: "open" });
+    // create shadowRoot and append nav
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.nav = this.shadowRoot.querySelector("nav");
+    this.activeLink = null;
 
-        // Binding
-        this.initNavLinks = this.initNavLinks.bind(this);
-        this.initObserver = this.initObserver.bind(this);
-        this.setActiveLink = this.setActiveLink.bind(this);
+    // Binding
+    this.initNavLinks = this.initNavLinks.bind(this);
+    this.initObserver = this.initObserver.bind(this);
+    this.setActiveLink = this.setActiveLink.bind(this);
+  }
+
+  connectedCallback() {
+    // here is where nav with links should be rendered
+    this.initNavLinks(links);
+    this.initObserver();
+    let mql = window.matchMedia("(max-width: 768px)");
+    if (mql.matches) {
+      gsap.fromTo(
+        this.nav,
+        { opacity: 0, y: 40 },
+        { opacity: 1, y: 0, ease: "Bounce.easeOut", duration: 1.25 }
+      );
+    } else {
+      gsap.fromTo(
+        this.nav,
+        { opacity: 0, y: -40 },
+        { opacity: 1, y: 0, ease: "Bounce.easeOut", duration: 1.25 }
+      );
     }
+  }
 
-    connectedCallback() {
-        // here is where nav with links should be rendered
-        this.initNavLinks(links);
-        this.initObserver();
-        let mql = window.matchMedia("(max-width: 768px)");
-        if (mql.matches) {
-            gsap.fromTo(
-                this.nav,
-                { opacity: 0, y: 40 },
-                { opacity: 1, y: 0, ease: "Bounce.easeOut", duration: 1.25 }
-            );
-        } else {
-            gsap.fromTo(
-                this.nav,
-                { opacity: 0, y: -40 },
-                { opacity: 1, y: 0, ease: "Bounce.easeOut", duration: 1.25 }
-            );
-        }
-    }
+  initNavLinks(array) {
+    array.forEach((link) => {
+      const el = document.createElement("nav-link");
+      el.link = link;
+      this.nav.appendChild(el);
+    });
+  }
 
-    initNavLinks(array){
-        links.forEach(link => {
-            const el = document.createElement('nav-link');
-            el.link = link;
-            this.nav.appendChild(el);
-        })
-    }
-
-    initObserver() {
-        // jména všech sekcí to observe
-        const observer = new IntersectionObserver(
-            (entries, observer) => {
-                // if we scroll over section
-                entries.forEach(entry => {
-                    if (entry.isIntersecting) {
-                        for (let i = 0; i < links.length ; i++) {
-                            entry.target.id === links[i].targetId && this.setActiveLink(i);
-                        }
-                    }
-                });
-            },
-            {
-                rootMargin: "0px 0px",
-                threshold: 0.8
+  initObserver() {
+    // jména všech sekcí to observe
+    const observer = new IntersectionObserver(
+      (entries, observer) => {
+        // if we scroll over section
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            for (let i = 0; i < links.length; i++) {
+              entry.target.id === links[i].targetId && this.setActiveLink(i);
             }
-        );
+          }
+        });
+      },
+      {
+        rootMargin: "0px 0px",
+        threshold: 0.8,
+      }
+    );
 
-        // vrať pole všech elementů s targetId a pro každý prvek pozoruj
-        links.map(e => document.getElementById(e.targetId))
-            .forEach(section => observer.observe(section));
+    // vrať pole všech elementů s targetId a pro každý prvek pozoruj
+    links
+      .map((e) => document.getElementById(e.targetId))
+      .forEach((section) => observer.observe(section));
 
-        const observer2 = new IntersectionObserver(
-            (entries, observer2) =>{
-                entries.forEach(entry => {
-                    entry.isIntersecting && gsap.to(
-                        entry.target,
-                        { opacity: 1, x: 0, ease: "back.out(1.4)", duration: 1.25 }
-                    );
-                });
-            },
-            {
-                rootMargin: "0px 0px",
-                threshold: 0.5
-            }
-        )
+    const observer2 = new IntersectionObserver(
+      (entries, observer2) => {
+        entries.forEach((entry) => {
+          entry.isIntersecting &&
+            gsap.to(entry.target, {
+              opacity: 1,
+              x: 0,
+              ease: "back.out(1.4)",
+              duration: 1.25,
+            });
+        });
+      },
+      {
+        rootMargin: "0px 0px",
+        threshold: 0.5,
+      }
+    );
 
-        const animArr = document.querySelectorAll('.animate');
-        animArr.forEach(el => observer2.observe(el));
+    const animArr = document.querySelectorAll(".animate");
+    animArr.forEach((el) => observer2.observe(el));
+  }
+
+  setActiveLink(index) {
+    // select link from list of nav-link elements
+    const navLinkArray = this.nav.querySelectorAll("nav-link");
+    navLinkArray[index].setAttribute("active", "");
+    if (!(this.activeLink === null)) {
+      this.activeLink.removeAttribute("active");
+      navLinkArray[index].setAttribute("active", "");
     }
-
-    setActiveLink(index){
-        // select link from list of nav-link elements
-        const navLinkArray = this.nav.querySelectorAll('nav-link');
-        if(this.activeLink === null){
-            navLinkArray[index].setAttribute('active', '')
-        } else {
-            this.activeLink.removeAttribute('active');
-            navLinkArray[index].setAttribute('active', '');
-        }
-        this.activeLink = navLinkArray[index];
-    }
+    this.activeLink = navLinkArray[index];
+  }
 }
-
 
 export default VetNavigation;
